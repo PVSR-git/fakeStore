154afc79361f8803e3c045c2299bfacac65a3acc
PVSKR <skr.polu@hotmail.com>
2023-04-10 17:07:43 +0530

    fakeStore app Begin

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+import { Store } from "./Components/Store";
+
+function App() {
+  return <Store />;
+}
+
+export default App;
---
+++ src/Components/Store.tsx
@@ -0,0 +1,3 @@
+export function Store() {
+  return <h3 className="text-warning p-4">Hi...</h3>;
+}
