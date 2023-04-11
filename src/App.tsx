898d2d33f4a26a8b40e61ca23331abb7cfd0fb12
PVSKR <skr.polu@hotmail.com>
2023-04-11 16:03:47 +0530

    Async/await use

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-
+import "./App.css";
 import Store from "./Components/Store";
 
 function App() {
--- src/Components/Store.tsx
+++ src/Components/Store.tsx
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-const apidata = "https://fakestoreapi.com/products ";
+
+const apidata = "https://fakestoreapi.com/products";
 export default function Store() {
   const [title, setTitle] = useState([]);
+  const [image, setImage] = useState([]);
+  const [description, setDescription] = useState([]);
+
   useEffect(() => {
     async function getStoreData() {
-      const response = await axios.get(apidata);
+      const response = await axios.get(`${apidata}/9`);
       console.log(response);
       setTitle(response.data.title);
+      setImage(response.data.image);
+      setDescription(response.data.description);
     }
     getStoreData();
   }, []);
   return (
     <>
-      <h3 className="text-warning p-4">Hi...</h3>
-      <div></div>
+      <h2 className="text-warning p-4">Hi...</h2>
+      <div>
+        <h2>{title}</h2>
+        <span>
+          <img src={image} style={{ width: "150px", height: "150px" }} />
+        </span>
+        <p>{description}</p>
+      </div>
     </>
   );
 }
