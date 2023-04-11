a7aa2904623ee4f705eadbabd547eb5499e41705
PVSKR <skr.polu@hotmail.com>
2023-04-11 15:21:43 +0530

    Console text
    I tested whether the data fetch link is working or not with the help of console.log.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
-import { Store } from "./Components/Store";
+import Store from "./Components/Store";
 
 function App() {
   return <Store />;
--- src/Components/Store.tsx
+++ src/Components/Store.tsx
@@ -1,3 +1,20 @@
-export function Store() {
-  return <h3 className="text-warning p-4">Hi...</h3>;
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const apidata = "https://fakestoreapi.com/products ";
+export default function Store() {
+  const [title, setTitle] = useState([]);
+  useEffect(() => {
+    async function getStoreData() {
+      const response = await axios.get(apidata);
+      console.log(response);
+      setTitle(response.data.title);
+    }
+    getStoreData();
+  }, []);
+  return (
+    <>
+      <h3 className="text-warning p-4">Hi...</h3>
+      <div></div>
+    </>
+  );
 }
